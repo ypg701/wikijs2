48db61ddf92c5106454c8af58a7faa6126a06826
carleri5 <119938092+carleri5@users.noreply.github.com>
2022-12-06 05:40:20 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #请修改为自己的UUID
-export UUID=56c7b99b-4ca3-406c-b39b-b2127a87f673
+export UUID=d4df7c9e-f676-4390-9e7b-c7610a70d13f
 
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=bing.com
+export ProxySite=js.wiki
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
